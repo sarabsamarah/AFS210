3e5b4b3f6bfe242a0bc3253ae88a61f35ea50621
sarabsamarah <sarab.samarah@bryanuniversity.edu>
2022-04-04 13:51:24 -0700

    Week 2 Python Data Structures

---
+++ week2/datastructures.py
@@ -0,0 +1,25 @@
+Data1 = 7, False, "Apple", True, 7, 98.6 
+print(len(Data1))
+Data2 = {"July", 4, 8, "Tango", 4.3, "Bingo"}
+Data2.pop()
+Data2.add("Alpha")
+print(Data2)
+Data3 = ["A", 7, -1, 3.14, True, 7]
+Data3.reverse()
+Data3[1]="8"
+Data3.pop()
+print(Data3)
+
+Data4 = {
+    "name" : "Joe",  
+    "age" : 26,   
+    "active" : True,  
+    "hourly_wage" : 14.75
+}
+print(Data4)
+Data4["active"] = False
+Data4["address"] = "123 Main Street"
+print(Data4["hourly_wage"]+ 40)
+print(Data4)
+
+ 
